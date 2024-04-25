007d770b512c99478dd353a644b84deb5dc25d0c
Diego Pizano <143430684+Krypticality@users.noreply.github.com>
2024-04-25 15:54:00 +0000

    images array

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 // Declare your arrays here
+let imageContainer = document.querySelector("#imageContainer");
+
+let comedy = ["https://i.pinimg.com/474x/ff/6b/fe/ff6bfe5338bf60e7295168aaca98e144.jpg",
+"https://www.filmsourcing.com/wp-content/uploads/2013/03/werethemillers-poster.jpg"];
+
+let action = ["https://www.movieposters.com/cdn/shop/products/ed4796ac6feff9d2a6115406f964c928_6b200bda-fe71-4900-ad7f-903cdda50dab_240x360_crop_center.progressive.jpg?v=1573587596", 
+"https://in.originalfilmart.com/cdn/shop/products/terminator_1984_italian_original_film_art_a.jpg?v=1597393983"];
+
+let drama = ["https://m.media-amazon.com/images/I/61hvjpjTZkL._AC_UF894,1000_QL80_.jpg",
+"https://www.filmposters.com/images/posters/4441.jpg"];
+
 
 // Make sure to declare your HTML elements as variables! 
 
